60ff774d84bad8fd2bac437e182eecd428cd3578
Jason Robitaille <jason.robitaille@lge.com>
2017-02-02 16:41:05 -0800

    Adjust prerender-time FileXHR for public path usage.

--- global-cli/modifiers/util/FileXHR.js
+++ global-cli/modifiers/util/FileXHR.js
@@ -16,11 +16,14 @@ FileXHR.prototype.addEventListener = function(evt, fn) {
 };
 
 FileXHR.prototype.send = function() {
+	if(global.publicPath && this.uri.indexOf(global.publicPath) === 0) {
+		this.uri = this.uri.substring(global.publicPath.length);
+	}
 	if(this.method.toUpperCase() === 'GET' && this.uri && this.sync) {
 		try {
 			var outUri = path.join(FileXHR.compilation.options.output.path, this.uri);
 			if(!exists(outUri)) throw new Error('File not found: ' + this.uri);
-			
+
 			this.response = this.responseText = fs.readFileSync(outUri, {encoding:'utf8'});
 			this.status = 200;
 			this.onload && this.onload();
