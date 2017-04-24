3ecc66a46273c52304517c6d9825189d7c62d982
Jason Robitaille <jason.robitaille@lge.com>
2017-04-24 13:32:24 -0700

    PLAT-40259: Fix lazy loading with prerendering by rerouting to require modules via NodeJS

--- global-cli/modifiers/util/vdom-server-render.js
+++ global-cli/modifiers/util/vdom-server-render.js
@@ -37,48 +37,72 @@ var context = vm.createContext(sandbox);
 		locale 			Specific locale to use in rendering
 		externals		filepath to external Enact framework to use with rendering
 */
-module.exports = function(opts) {
-	var rendered;
+module.exports = {
+	prepare: function(code, opts) {
+		code = code.replace(/window\["webpackJsonpApp"\]/g, 'global["webpackJsonpApp"]');
 
-	if(opts.locale) {
-		sandbox.XMLHttpRequest = FileXHR;
-	} else {
-		delete sandbox.XMLHttpRequest;
-	}
+		var start = code.indexOf('__webpack_require__.e =');
+		if(start>=0) {
+			start = code.indexOf('var head = document', start);
+			if(start>=0) {
+				var end = code.indexOf('};', start);
+				if(end>=0) {
+					code = code.substring(0, start) + 'require(require("path").resolve(' + code.match(/\.src = ([^;]*);/)[1]
+							+ '));}' + code.substring(end);
+				}
+			}
+		}
+		require('fs').writeFileSync('debug.js', code, {encoding:'utf8'});
 
-	try {
-		console.mute();
 
 		if(opts.externals) {
-			opts.externals = path.resolve(path.join(opts.externals, 'enact.js'));
 			// Add external Enact framework filepath if it's used.
-			opts.code = opts.code.replace(/require\(["']enact_framework["']\)/g, 'require("' + opts.externals +  '")');
-			// Ensure locale switching  support is loaded globally with external framework usage.
-			var framework = require(opts.externals);
-			sandbox.iLibLocale = framework('@enact/i18n/locale');
+			code = code.replace(/require\(["']enact_framework["']\)/g, 'require("'
+					+ path.resolve(path.join(opts.externals, 'enact.js')) +  '")');
+		}
+		return code;
+	},
+
+	render: function(opts) {
+		var rendered;
+
+		if(opts.locale) {
+			sandbox.XMLHttpRequest = FileXHR;
 		} else {
-			delete sandbox.iLibLocale
+			delete sandbox.XMLHttpRequest;
 		}
 
-		m.exports = {};
-		vm.runInContext(opts.code, context, {
-			filename: opts.file,
-			displayErrors: true
-		});
-
-		// Update locale if needed.
-		if(opts.locale && sandbox.iLibLocale && sandbox.iLibLocale.updateLocale) {
-			console.resume();
-			sandbox.iLibLocale.updateLocale(opts.locale);
+		try {
 			console.mute();
+
+			if(opts.externals) {
+				// Ensure locale switching  support is loaded globally with external framework usage.
+				var framework = require(path.resolve(path.join(opts.externals, 'enact.js')));
+				sandbox.iLibLocale = framework('@enact/i18n/locale');
+			} else {
+				delete sandbox.iLibLocale
+			}
+
+			m.exports = {};
+			vm.runInContext(opts.code, context, {
+				filename: opts.file,
+				displayErrors: true
+			});
+
+			// Update locale if needed.
+			if(opts.locale && sandbox.iLibLocale && sandbox.iLibLocale.updateLocale) {
+				console.resume();
+				sandbox.iLibLocale.updateLocale(opts.locale);
+				console.mute();
+			}
+
+			rendered = opts.server.renderToString(m.exports['default'] || m.exports);
+
+			console.resume();
+		} catch(e) {
+			console.resume();
+			throw e;
 		}
-
-		rendered = opts.server.renderToString(m.exports['default'] || m.exports);
-
-		console.resume();
-	} catch(e) {
-		console.resume();
-		throw e;
+		return rendered;
 	}
-	return rendered;
 };
